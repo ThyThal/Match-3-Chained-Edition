e238eac55c15f48b3a2518fcf55be2839f51cfd6
Alejo Gaitán <thy.gaitan@gmail.com>
2021-06-25 03:12:42 -0300

    FSM Main Logic

---
+++ Match 3 (Chained Edition)/Assets/_Scripts/FSM/FSM.cs
@@ -0,0 +1,35 @@
+﻿public class FSM<T>
+{
+    private FSMState<T> _currentState;
+
+    /*
+     * FSM Methods
+     */
+    public void SetInitState(FSMState<T> initialState)
+    {
+        _currentState = initialState;
+        _currentState.Awake();
+    }
+    public void OnUpdate()
+    {
+        if (_currentState != null)
+        {
+            _currentState.Execute();
+        }
+    }
+    public void Transition(T input)
+    {
+        FSMState<T> newState = _currentState.GetTransition(input);
+
+        if (newState != null)
+        {
+            _currentState.Sleep();
+            _currentState = newState;
+            _currentState.Awake();
+        }
+    }
+    public FSMState<T> GetCurrentState()
+    {
+        return _currentState;
+    }
+}
---
+++ Match 3 (Chained Edition)/Assets/_Scripts/FSM/FSMState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+public class FSMState<T>
+{
+    private Dictionary<T, FSMState<T>> _states = new Dictionary<T, FSMState<T>>();
+
+    /*
+     * FSM State Functions
+     */
+    public virtual void Awake() { }
+    public virtual void Execute() { }
+    public virtual void Sleep() { }
+
+    /*
+     * FSM State Methods
+     */
+    public void AddTransition(T input, FSMState<T> state)
+    {
+        if (!_states.ContainsKey(input))
+        {
+            _states.Add(input, state);
+        }
+    }
+    public void RemoveTransition(T input)
+    {
+        if (_states.ContainsKey(input))
+        {
+            _states.Remove(input);
+        }
+    }
+    public FSMState<T> GetTransition(T input)
+    {
+        if (_states.ContainsKey(input))
+        {
+            return _states[input];
+        }
+
+        else
+        {
+            return null;
+        }
+    }
+}
